4d24431539b3d1fce4dfb872fd0a347e48919d94
Mohamed Yassin <mohamed.yassing@hotmail.com>
2025-07-09 20:03:56 +0400

    Grid Layout
    testing for dynamic grid layout for different game modes (2x2 ,2x3 ,etc..)

---
+++ MohamedYassinMatchingCards/Assets/Scripts/GridLayoutController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridLayoutController : MonoBehaviour
+{
+    public int rows;
+    public int columns;
+
+    private RectTransform rectTransform;
+    private GridLayoutGroup gridLayout;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        gridLayout = GetComponent<GridLayoutGroup>();
+    }
+
+    public void ConfigureGrid(int newRows, int newColumns)
+    {
+        rows = newRows;
+        columns = newColumns;
+
+        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayout.constraintCount = columns;
+
+        float width = rectTransform.rect.width;
+        float height = rectTransform.rect.height;
+
+        float spacingX = gridLayout.spacing.x;
+        float spacingY = gridLayout.spacing.y;
+
+        float totalSpacingX = spacingX * (columns - 1);
+        float totalSpacingY = spacingY * (rows - 1);
+
+        float cellWidth = (width - totalSpacingX) / columns;
+        float cellHeight = (height - totalSpacingY) / rows;
+
+        float size = Mathf.Min(cellWidth, cellHeight);
+        gridLayout.cellSize = new Vector2(size, size);
+    }
+}
